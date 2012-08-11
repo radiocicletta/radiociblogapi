cc01f62ce22aaabf04640282d790cc12c823597e
Leonardo <leofiore@libero.it>
2012-08-11 19:13:22 +0200

    first commit

---
+++ lastfm.py
@@ -0,0 +1,8 @@
+#!/bin/env python
+
+import urllib2
+import json
+import sys, os
+
+def harvest(path, idxname):
+    pass
---
+++ mixcloud.py
@@ -0,0 +1,45 @@
+#!/bin/env python
+
+import urllib2
+import json
+import sys, os
+
+def harvest(path, idxname):
+
+    base_url = "http://api.mixcloud.com"
+    username = "radiocicletta"
+
+    jsonidx = open("%s/%s.json" % (path, idxname), 'w')
+    jsonidxobj = { "name": idxname, "playlists":{} }
+
+    if not os.path.exists("%s/%s" % (path, idxname)):
+        os.makedirs("%s/%s" % (path, idxname))
+
+    try:
+        result = urllib2.urlopen("%s/%s/playlists/" % (base_url, username))
+        playlists = json.loads(result.read())
+    except:
+        playlists =  { data:[] }
+
+
+    for plist in playlists["data"]:
+        js = open("%s/%s/%s.json" % (path, idxname, plist['slug']), 'w')
+        try:
+            result = urllib2.urlopen("%s/%s/playlists/%s/cloudcasts/" % (base_url, username, plist['slug']))
+            js.write(result.read())
+            jsonidxobj["playlists"][plist["slug"]] = "%s/%s.json" % (idxname, plist['slug'])
+        except:
+            pass
+        finally:
+            js.close()
+
+    jsonidx.write(json.dumps(jsonidxobj))
+    jsonidx.close()
+
+if __name__ == "__main__":
+    if not len(sys.argv):
+        sys.exit(0)
+
+    path = sys.argv[1]
+    idx = sys.argv[2]
+    harvest(path, idx)
