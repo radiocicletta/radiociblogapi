7c80732e0ea9e281f3a152c09e7818955d401c8a
leonardo <leofiore@libero.it>
2014-08-22 20:00:32 +0200

    More randomness in the photos selection

--- fbcover.py
+++ fbcover.py
@@ -25,7 +25,8 @@ def getalbums(**kwargs):
             (app_id, app_secret))
         txt = oauth.read()
         access_token = re.search('access_token=(.*)', txt).groups()[0]
-    except:
+    except Exception as e:
+        print e
         sys.exit(0)
 
     facebook = hammock.Hammock("https://graph.facebook.com")
@@ -52,60 +53,64 @@ def getalbums(**kwargs):
                     u'Timeline Photos'
                 )]
             shuffle(filtered)
+            items = []
             for album in filtered:
                 photos = facebook.__getattr__(album['id']).GET(
                     "photos", params={"access_token": access_token}
                 ).json()
-                shuffle(photos['data'])
                 chosen = sample(
                     photos['data'],
                     choice(range(1, int(math.ceil(len(filtered) ** 0.5))))
                 )
-                for c in chosen:
-                    print "processing", c['source']
-                    try:
-                        data = StringIO(urllib2.urlopen(c['source']).read())
-                        img = Image.open(data)
-                        if choice((0, 1)):
-                            img = img.resize((
-                                img.size[0] / 2,
-                                img.size[1] / 2),
-                                Image.ANTIALIAS)
-                        if img.size[0] < img.size[1]:  # portrait
-                            img = img.crop((
-                                max(img.size[0] / 2 - 120 * choice((1, 2)), 0),
-                                max(img.size[1] / 2 - 105 * choice((1, 2, 3)), 0),
-                                img.size[0] / 2 + 120,
-                                img.size[1] / 2 + 105,
-                            ))
-                        else:  # landscape
-                            img = img.crop((
-                                max(img.size[0] / 2 - 120 * choice((1, 2, 3)), 0),
-                                max(img.size[1] / 2 - 105 * choice((1, 2)), 0),
-                                img.size[0] / 2 + 120,
-                                img.size[1] / 2 + 105,
-                            ))
-                        draw = ImageDraw(img)
-                        draw.rectangle(
-                            [1, 1,
-                             img.size[0] - 2, img.size[1] - 2],
-                            outline="white"
-                        )
-                        draw.rectangle(
-                            [0, 0,
-                             img.size[0] - 1, img.size[1] - 1],
-                            outline="white"
-                        )
-                        images.append(
-                            Rectangle(
-                                0, 0,
-                                img.size[0],
-                                img.size[1],
-                                img
-                            ))
-                    except:
-                        pass
-        except:
+                items.extend(chosen)
+            shuffle(items)
+            for c in items:
+                print "processing", c['source']
+                try:
+                    data = StringIO(urllib2.urlopen(c['source']).read())
+                    img = Image.open(data)
+                    if choice((0, 1)):
+                        img = img.resize((
+                            img.size[0] / 2,
+                            img.size[1] / 2),
+                            Image.ANTIALIAS)
+                    if img.size[0] < img.size[1]:  # portrait
+                        img = img.crop((
+                            max(img.size[0] / 2 - 120 * choice((1, 2)), 0),
+                            max(img.size[1] / 2 - 105 * choice((2, 3)), 0),
+                            min(img.size[0] / 2 + 120, img.size[0]),
+                            min(img.size[1] / 2 + 105, img.size[1]),
+                        ))
+                    else:  # landscape
+                        img = img.crop((
+                            max(img.size[0] / 2 - 120 * choice((2, 3)), 0),
+                            max(img.size[1] / 2 - 105 * choice((1, 2)), 0),
+                            min(img.size[0] / 2 + 120, img.size[0]),
+                            min(img.size[1] / 2 + 105, img.size[1]),
+                        ))
+                    draw = ImageDraw(img)
+                    draw.rectangle(
+                        [1, 1,
+                         img.size[0] - 2, img.size[1] - 2],
+                        outline="white"
+                    )
+                    draw.rectangle(
+                        [0, 0,
+                         img.size[0] - 1, img.size[1] - 1],
+                        outline="white"
+                    )
+                    images.append(
+                        Rectangle(
+                            0, 0,
+                            img.size[0],
+                            img.size[1],
+                            img
+                        ))
+                except Exception as e:
+                    print e
+                    #pass
+        except Exception as e:
+            print e
             sys.exit(0)
 
         container = Rectangle(
